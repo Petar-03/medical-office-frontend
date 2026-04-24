e046be0751560fc3eb57ac8af103b3d322f76ca2
Petar-03 <petar_ganchev03@abv.bg>
2026-04-24 20:07:46 +0300

    added real doctor image and made notifications

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import doctorImage from "../assets/doctor-ivanova.png";
 function Topbar() {
+  const [showNotifications, setShowNotifications] = useState(false);
+
   return (
     <header className="topbar">
       <h1>Начало</h1>
@@ -9,8 +13,25 @@ function Topbar() {
       </div>
 
       <div className="doctor-box">
-        <span className="bell">🔔</span>
-        <div className="avatar">👩‍⚕️</div>
+        <div className="notification-wrapper">
+          <button
+            className="bell"
+            onClick={() => setShowNotifications(!showNotifications)}
+          >
+            🔔
+          </button>
+
+          {showNotifications && (
+            <div className="notifications-box">
+              <h4>Известия</h4>
+
+              <p>Имате 3 предстоящи часа.</p>
+              <p>Имате 2 нови пациента.</p>
+              <p>Проверете графика за деня.</p>
+            </div>
+          )}
+        </div>
+        <img src={doctorImage} alt="Д-р Иванова" className="avatar" />
         <span>Д-р Иванова</span>
       </div>
     </header>
